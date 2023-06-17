1d2a40b4527d83433cd42cfc05acefc9b5e4575e
Deorren Erive <erive92d@gmail.com>
2023-06-17 11:35:42 -0700

    s

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 // Import the ApolloServer class
 const { ApolloServer } = require("apollo-server-express");
