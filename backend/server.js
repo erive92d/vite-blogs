e0454769256e7cc3fb6857fa3de145cb27ccd3b7
Deorren Erive <erive92d@gmail.com>
2023-06-17 12:28:13 -0700

    s

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors());
 // );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 }
 
 app.get("/", (req, res) => {
