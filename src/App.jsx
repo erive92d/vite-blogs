124430d0085ae9601637155f47b7e38366a6ba20
Deorren Erive <erive92d@gmail.com>
2023-06-17 12:32:53 -0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Profile from './pages/Profile/Profile';
 import SinglePost from './pages/SinglePost';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
